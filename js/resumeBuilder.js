5495a5eedb6e49c8014b689e62486d874fcc41f4
HackingBuddha <algis999@yahoo.com>
2015-10-07 21:07:56 +0300

    Add skills

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -4,9 +4,10 @@ var awesomeThoughts = "My name is Algis and I am AWESOME!";
 var funThoughts = awesomeThoughts.replace("AWESOME!", "FUN");
 var name = "Algirdas Bankauskas";
 var role = "Front-End Developer";
-var contact = "James Webb";
 var formattedName = HTMLheaderName.replace("%data%", name);
 var formattedRole = HTMLheaderRole.replace("%data%", role);
+var skills = ["HTML ", "CSS ", "Javascript"];
 
 $("#header").prepend(formattedRole);
 $("#header").prepend(formattedName);
+$("#main").append(skills);
