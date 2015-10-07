d031874b633513bfaed5ff3d78f4f95bdfe88616
HackingBuddha <algis999@yahoo.com>
2015-10-07 16:18:29 +0300

    Add name and role to the page

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -2,5 +2,10 @@ var email = "cameron@udacity.com";
 var newEmail = email.replace("udacity", "gmail");
 var awesomeThoughts = "My name is Algis and I am AWESOME!";
 var funThoughts = awesomeThoughts.replace("AWESOME!", "FUN");
+var name = "Algirdas Bankauskas";
+var role = "Front-End Developer";
+var formattedName = HTMLheaderName.replace("%data%", name);
+var formattedRole = HTMLheaderRole.replace("%data%", role);
 
-$("#main").append(funThoughts);
+$("#main").prepend(formattedName);
+$("#main").append(formattedRole);
