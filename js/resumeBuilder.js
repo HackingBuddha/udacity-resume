a8012da485724f40aa191a0a5e03cd083082c20f
HackingBuddha <algis999@yahoo.com>
2015-10-07 16:04:50 +0300

    Udacity Resume Project
    A Project based on Udacitys Front-End Nanondegree Resume

---
+++ js/resumeBuilder.js
@@ -0,0 +1,6 @@
+var email = "cameron@udacity.com";
+var newEmail = email.replace("udacity", "gmail");
+var awesomeThoughts = "My name is Algis and I am AWESOME!";
+var funThoughts = awesomeThoughts.replace("AWESOME!", "FUN");
+
+$("#main").append(funThoughts);
